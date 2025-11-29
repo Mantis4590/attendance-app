1ee9eb018766a7c2c47e7aec547daca48de4db53
nishida ryoma <hb23a080@oecu.jp>
2025-11-30 00:32:00 +0900

    画面作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\StampCorrectionRequestController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
+Route::get('/attendance/list', [AttendanceController::class, 'list'])->name('attendance.list');
+Route:: get('/attendance/detail/{id}', [AttendanceController::class, 'detail'])
+    ->name('attendance.detail');
+Route::get('/stamp_correction_request/list', [StampCorrectionRequestController::class, 'list'])
+    ->name('request.list');
+Route::post('/logout', function() {
+    Auth::logout();
+    return redirect('/login');
+})->name('logout');
+Route::post('/register', [AuthController::class, 'store'])->name('register.store');
+Route::post('/login', [AuthController::class, 'loginStore'])->name('login.store');
+Route::post('/attendance/clock-in', [AttendanceController::class, 'clockIn'])
+    ->name('attendance.clockIn');
+
+Route::post('/attendance/clock-out', [AttendanceController::class, 'clockOut'])
+    ->name('attendance.clockOut');
+
+Route::post('/attendance/start-break', [AttendanceController::class, 'startBreak'])
+    ->name('attendance.startBreak');
+
+Route::post('/attendance/end-break', [AttendanceController::class, 'endBreak'])
+    ->name('attendance.endBreak');
